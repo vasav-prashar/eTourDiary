d9aef9814a6e76a9287d0ced50516c88083411c5
ChesterKing-19 <sahilqm2001@outlook.com>
2023-05-03 01:50:26 +0530

    feat: improve bottom navbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: use_build_context_synchronously, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:etourdiary/animations/customAnimation.dart';
 import 'package:etourdiary/pages/auth/login.dart';
 import 'package:etourdiary/pages/download.dart';
 import 'package:etourdiary/pages/splash_screen.dart';
@@ -7,6 +10,7 @@ import 'view.dart';
 import 'submit.dart';
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -21,79 +25,84 @@ class _HomeState extends State<Home> {
 
   int _selectedIndex = 0;
 
-  List<Widget> pages = [MyStatefulWidget(), View(), Download()];
+  List<Widget> pages = [Submit(), View(), Download()];
 
   void signOut() async {
     await FirebaseAuth.instance.signOut();
     var sharedPref = await SharedPreferences.getInstance();
     sharedPref.setBool(SplashScreenState.KEYLOGIN, false);
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => Login()));
+    Navigator.pushReplacement(context, CustomPageRoute(child: const Login()));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
       title: Home._title,
-      home: Scaffold(
-        drawer: Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
-              UserAccountsDrawerHeader(
-                currentAccountPicture: Icon(Icons.person),
-                accountEmail: Text('${_auth.currentUser?.email}'),
-                accountName: Text('${_auth.currentUser?.displayName}'),
-                decoration: BoxDecoration(
-                  color: Colors.black45,
-                ),
+      home: Container(
+        // decoration: const BoxDecoration(
+        //   gradient: LinearGradient(
+        //       begin: Alignment.topRight,
+        //       end: Alignment.bottomLeft,
+        //       colors: [
+        //         Color.fromRGBO(76, 80, 91, 1),
+        //         Color.fromRGBO(94, 98, 108, 1)
+        //       ],
+        //     )),
+        child: Scaffold(
+          // backgroundColor: Colors.transparent,
+            drawer: Drawer(
+              // backgroundColor: Color(0xff4c505b),
+              // Add a ListView to the drawer. This ensures the user can scroll
+              // through the options in the drawer if there isn't enough vertical
+              // space to fit everything.
+              child: ListView(
+                // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: [
+                  UserAccountsDrawerHeader(
+                    currentAccountPicture: Icon(Icons.person),
+                    accountEmail: Text('${_auth.currentUser?.email}'),
+                    accountName: Text('${_auth.currentUser?.displayName}'),
+                    decoration: const BoxDecoration(
+                      color: Colors.black45,
+                    ),
+                  ),
+                  ListTile(
+                    title: const Text('LogOut'),
+                    onTap: () {
+                      signOut();
+                      // Update the state of the app
+                      // ...
+                      // Then close the drawer
+                    },
+                  ),
+                ],
               ),
-              ListTile(
-                title: const Text('LogOut'),
-                onTap: () {
-                  signOut();
-                  // Update the state of the app
-                  // ...
-                  // Then close the drawer
-                },
-              ),
-            ],
-          ),
-        ),
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(title: const Text(Home._title)),
-        body: DoubleBackToCloseApp(
-          child: pages.elementAt(_selectedIndex),
-          snackBar: const SnackBar(
-            content: Text('Tap back again to Exit'),
-          ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: const Icon(Icons.add_circle),
-              label: "Submit",
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.view_agenda), label: "View"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.download), label: "Download")
-          ],
-          currentIndex: _selectedIndex,
-          onTap: (index) => {
-            setState(
-              () {
-                _selectedIndex = index;
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(title: const Text(Home._title),backgroundColor: Color(0xff4c505b)),
+            body: DoubleBackToCloseApp(
+              snackBar: const SnackBar(
+                content: Text('Tap back again to Exit'),
+              ),
+              child: pages.elementAt(_selectedIndex),
+            ),
+            bottomNavigationBar: ConvexAppBar(
+              items: [
+                TabItem(icon: Icons.add, title: 'Submit'),
+                TabItem(icon: Icons.view_agenda, title: 'View'),
+                TabItem(icon: Icons.download, title: 'Download'),
+              ],
+              backgroundColor: Color(0xff4c505b),
+              color: Colors.white,
+              height: 50,
+              onTap: (index) => {
+                setState(() {
+                  _selectedIndex = index;
+                })
               },
-            )
-          },
-        ),
+            )),
       ),
     );
   }
